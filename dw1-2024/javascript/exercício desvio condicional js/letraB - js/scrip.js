6e24c482ebc587ffd3524a4e2c63599528541626
Anna <annafelferrer@gmail.com>
2024-09-07 15:08:23 -0300

    tarefa

---
+++ dw1-2024/javascript/exercício desvio condicional js/letraB - js/scrip.js
@@ -0,0 +1,22 @@
+let condicao1 = true
+let condicao2 = false
+let condicao3 = false
+
+console.log("1")
+console.log("2")
+
+if (condicao1 == true){
+    console.log("A")
+} else {
+    if (condicao2 == false){
+        console.log("D")
+    } else{
+        if (condicao3 = true){
+            console.log("B")
+        } else{
+            console.log("C")
+        }
+    }
+}
+console.log("3")
+console.log("4")
